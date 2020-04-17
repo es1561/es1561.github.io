cb1d1fce25ac259a2b1bf5e8bba74ef2b28f3301
Luis Fernando Monção Guimarães <mega_710@hotmail.com>
2020-04-17 18:32:42 -0300

    meh

--- src/java/Servlet/Search.java
+++ src/java/Servlet/Search.java
@@ -90,7 +90,7 @@ public class Search extends HttpServlet
                         @Override
                         public int compare(Item o1, Item o2)
                         {
-                            return o1.getTime().compareTo(o2.getTime());
+                            return o1.getTime().compareTo(o2.getTime()) * -1;
                         }
                     });
                     
